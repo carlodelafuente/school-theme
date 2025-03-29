0678ac5739a8555b2a012a6817244e58aebd949f
Carlo dela Fuente <gdelafuente1@my.bcit.ca>
2025-03-28 22:31:12 -0700

    add student cpt and student taxonomy

--- functions.php
+++ functions.php
@@ -19,6 +19,29 @@ function school_enqueues() {
 
 
 }
-
-
 add_action('wp_enqueue_scripts', 'school_enqueues');
+
+function school_setup() {
+
+	add_editor_style( get_stylesheet_uri() );
+
+
+    add_image_size( '400x500', 400, 500, true );
+    add_image_size( '200x250', 200, 250, true );
+    add_image_size( '400x200', 400, 200, true );
+    add_image_size( '800x400', 800, 400, true );
+}
+add_action( 'after_setup_theme', 'school_setup' );
+
+function school_add_custom_image_sizes( $size_names ) {
+	$new_sizes = array(
+		'400x500' => __( '400x500', 'school-theme' ),
+		'200x250' => __( '200x250', 'school-theme' ),
+        '400x200' => __( '400x200', 'school-theme' ),
+		'800x400' => __( '800x400', 'school-theme' ),
+	);
+	return array_merge( $size_names, $new_sizes );
+}
+add_filter( 'image_size_names_choose', 'school_add_custom_image_sizes' );
+
+require get_template_directory() . '/inc/post-types-taxonomies.php';
